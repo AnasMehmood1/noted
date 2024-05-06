b366d3d25d8398c4b9f5fc999475d4364e4fee94
AnasMehmood1 <anasmehmood167@gmail.com>
2024-05-06 22:43:34 +0500

    done

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,13 @@ const port = 5000
 
 
 
-app.use(cors())
+app.use(cors(
+  {
+    origin:[""],
+    methods:["POST","GET"],
+    credentials:true
+  }
+))
 app.use(express.json())
 
 app.use('/api/auth',require('./routes/auth'))
